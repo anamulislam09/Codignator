37c0dbe3852fbf628593864113fce6413d76da75
Anamul-09 <mdanamulislam09@gmail.com>
2022-12-01 19:03:05 +0600

    first commit

---
+++ ci_app1/app/Controllers/Myhome.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Myhome extends BaseController
+{
+    public function index()
+    {
+        return view('contact');
+    }
+
+    public function about()
+    {
+        return view('about_us');
+    }
+    public function contact()
+    {
+        return view('contact');
+    }
+}
---
+++ ci_app1/app/Views/contact.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h1>This is a contact page</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, cupiditate ratione odit doloremque fuga pariatur ex rem vitae est beatae tenetur ullam doloribus cum qui voluptatem sapiente laborum tempora aliquam.</p>
+</body>
+
+</html>
